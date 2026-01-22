9b4f11088e464c89f911860ff344750bc087041b
amadeonot <amadeoriveraherrera@gmail.com>
2026-01-22 14:13:40 -0600

    Implementacion del Scheduler fcfs

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -10,8 +10,33 @@ int main() {
     read_processes(p, n);
     init_processes(p, n);
 
-    // TODO: Aquí va la lógica del scheduler PRUEBA PRUEBA
-    
+    /* FCFS Scheduling */
+
+    // Ordenar por tiempo de llegada
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].arrival_time > p[j].arrival_time) {
+                Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+
+    int current_time = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (current_time < p[i].arrival_time) {
+            current_time = p[i].arrival_time;
+        }
+
+        p[i].waiting_time = current_time - p[i].arrival_time;
+        p[i].turnaround_time = p[i].waiting_time + p[i].burst_time;
+
+        current_time += p[i].burst_time;
+        p[i].completed = 1;
+    }
+
     print_results(p, n, "FCFS Scheduling");
     return 0;
 }
