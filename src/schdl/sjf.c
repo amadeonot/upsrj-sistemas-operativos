b3528516df05e9d461fd03e049f73048b3ab98e9
amadeonot <amadeoriveraherrera@gmail.com>
2026-01-24 22:55:41 -0600

    implementacion sjf

--- src/schdl/sjf.c
+++ src/schdl/sjf.c
@@ -4,11 +4,38 @@
 /* ============================================================
  * Student implementation area
  * ============================================================ */
-void sjf_schedule(Process p[], int n)
+void sjf(Process p[], int n)
 {
-    (void)p;
-    (void)n;
-    /* TODO: Implement SJF scheduling algorithm here */
+    int time = 0;
+    int completed = 0;
+
+    while (completed < n) {
+        int idx = -1;
+        int min_burst = 1e9;
+
+        // Buscar el proceso disponible con menor burst_time
+        for (int i = 0; i < n; i++) {
+            if (!p[i].completed && p[i].arrival_time <= time) {
+                if (p[i].burst_time < min_burst) {
+                    min_burst = p[i].burst_time;
+                    idx = i;
+                }
+            }
+        }
+
+        // Si no hay procesos disponibles, avanzar el tiempo
+        if (idx == -1) {
+            time++;
+            continue;
+        }
+
+        // Ejecutar el proceso seleccionado
+        p[idx].waiting_time = time - p[idx].arrival_time;
+        time += p[idx].burst_time;
+        p[idx].turnaround_time = p[idx].waiting_time + p[idx].burst_time;
+        p[idx].completed = 1;
+        completed++;
+    }
 }
 
 /* ============================================================
@@ -25,43 +52,7 @@ int main(void)
     read_processes(p, n);
     init_processes(p, n);
 
-<<<<<<< HEAD
-    // TODO: Aquí va la lógica del scheduler
-    int time = 0;
-    int completed = 0;
-
-    while (completed < n) {
-        int idx = -1;
-        int min_bt = 1000000;
-
-        for (int i = 0; i < n; i++) {
-            if (!p[i].finished && p[i].at <= time) {
-                if (p[i].bt < min_bt) {
-                    min_bt = p[i].bt;
-                    idx = i;
-                }
-            }
-        }
-
-        if (idx == -1) {
-            time++;
-        } else {
-            time += p[idx].bt;
-            p[idx].ct = time;
-            p[idx].tat = p[idx].ct - p[idx].at;
-            p[idx].wt = p[idx].tat - p[idx].bt;
-            p[idx].finished = 1;
-            completed++;
-        }
-    }
-=======
-    sjf_schedule(p, n);
->>>>>>> e295d968ed0cea49e0d03f3c31992a399f5359b8
-
     print_results(p, n, "SJF Scheduling");
     return 0;
 }
-<<<<<<< HEAD
-=======
 #endif
->>>>>>> e295d968ed0cea49e0d03f3c31992a399f5359b8
